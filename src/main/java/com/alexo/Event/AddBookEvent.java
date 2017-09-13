61a9cb2db6cb598e60ff54cdb4cf13b507a46604
alex <alex>
2017-09-13 11:38:41 +0100

    Command Resource working for adding a batch of books

---
+++ src/main/java/com/alexo/Event/AddBookEvent.java
@@ -0,0 +1,47 @@
+package com.alexo.Event;
+
+import com.alexo.api.Book;
+import com.alexo.jdbi.PostgresDAO;
+import com.alexo.jdbi.ReadDAO;
+import com.google.gson.Gson;
+
+import java.text.SimpleDateFormat;
+
+public class AddBookEvent {
+    Book book;
+    Gson gson = new Gson();
+    String json;
+
+    PostgresDAO postgresDAO;
+    ReadDAO readDAO;
+
+    public AddBookEvent(Book book, PostgresDAO postgresDAO, ReadDAO readDAO) {
+        this.book = book;
+        this.postgresDAO = postgresDAO;
+        this.readDAO = readDAO;
+
+        json = gson.toJson(book);
+    }
+
+    public void addEvent() {
+
+        int failed =0;
+        int added =0;
+
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+    //String bookJson = gson.toJson(currentBook);
+        if (readDAO.findBook(book.getIsbn()) != 0) {
+            failed += 1;
+        } else {
+            postgresDAO.createFunc();
+            postgresDAO.triggerFunc();
+            postgresDAO.createTrigger();
+            postgresDAO.insertEvent(book.getIsbn(), "CREATED", json, timeStamp);
+            added += 1;
+        }
+
+        /*return "Books Added = " + added + '\n' +
+                "Number failed = " + failed;*/
+
+    }
+}
